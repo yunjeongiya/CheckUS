8090aa9d433a2e00cb2cafd131165d21801a13e2
윤정 <yunjeongiya@gmail.com>
2025-05-01 14:53:51 +0900

    Add Permission entity class for backend

---
+++ backend/src/main/java/com/checkus/domain/Permission.java
@@ -0,0 +1,29 @@
+package com.checkus.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "permission")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String name;
+    
+    @ManyToMany(mappedBy = "permissions")
+    private Set<Role> roles = new HashSet<>();
+}
